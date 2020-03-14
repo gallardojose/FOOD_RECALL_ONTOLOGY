a7d6dddb5c6e69a5f2f2adcb3c8d7bde9894ed29
annlm <annlm@uci.edu>
2020-03-14 18:26:35 +0100

    read json files

---
+++ project2.py
@@ -0,0 +1,10 @@
+import json
+
+# read the json files into python dictionaries
+
+with open("food-enforcement-0001-of-0001.json") as file:
+    food_recall = json.load(file)
+    
+with open("food-event-0001-of-0001.json") as file:
+    food_event = json.load(file)
+

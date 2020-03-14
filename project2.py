a157cf52c6f886441313308557741bb39dc99e42
Jose Gallardo <gallardojose444@gmail.com>
2020-03-14 13:15:39 -0700

    iterate event/recall data

--- project2.py
+++ project2.py
@@ -1,5 +1,6 @@
 import json
 
+
 # read the json files into python dictionaries
 
 with open("food-enforcement-0001-of-0001.json") as file:
@@ -7,3 +8,22 @@ with open("food-enforcement-0001-of-0001.json") as file:
     
 with open("food-event-0001-of-0001.json") as file:
     food_event = json.load(file)
+
+# iterate through event file
+# check against recalls for similar product description to name_brand
+# add similar recall and  products to owl, delete recalls from original json files
+# add product to product list to check against (repeated name_brand)
+
+name_brand_names = []
+i = 0
+for event in food_event["results"]:
+    for product in event["products"]:
+        for recall in food_recall["results"]:
+            print(str(product["name_brand"]) + " ")
+            print(recall["product_description"])
+            break
+        break
+    if i < 2:
+        i += 1
+    else:
+        break

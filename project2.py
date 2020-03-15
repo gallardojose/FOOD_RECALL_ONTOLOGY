8002c09347144d40777d181cd4c1410b971dd3a6
Jose Gallardo <gallardojose444@gmail.com>
2020-03-14 19:31:23 -0700

    add individual mishaps method
    working on adding individual mishaps to owl

--- project2.py
+++ project2.py
@@ -10,7 +10,34 @@ def compare_strings(str1, str2):
         if word in str2_list:
             in_string += 1
     # return word match percentage and length of name_brand
-    return in_string/len(str1_list), len(str1_list)
+    if len(str1_list) > 0:
+        return in_string/len(str1_list), len(str1_list)
+    else:
+        return 0, 0
+
+# def add_food_mishap(mishap, added):
+#     # open owl file and add individual mishap
+#     # start mishap
+#     rdf_mishap_opentag(mishap["report_number"])
+#     # add list of reactions, report_number, list of outcomes, list of products
+#     for reaction in mishap["reactions"]:
+#         added.write(rdf_reaction(reaction)) # ask for insertion of newline
+#     added.write(rdf_report_number(mishap["report_number"]))
+#     for outcome in mishap["outcomes"]:
+#         added.write(rdf_outcome(outcome))  # ask for insertion of newline
+#     for product in mishap["products"]:
+#         added.write(rdf_product_name_brand[product]["name_brand"])
+#         added.write(rdf_product_industry_code[product]["industry_code"])
+#         added.write(rdf_product_role[product]["role"])
+#         added.write(rdf_product_industry_name[product]["industry_name"])
+#     added.write(rdf_date_created(mishap["date_created"]))
+#
+#
+#     rdf_mishap_closetag()
+
+
+
+# def add_food_recall():
 
 
 # read the json files into python dictionaries
@@ -19,6 +46,9 @@ with open("food-enforcement-0001-of-0001.json") as file:
 
 with open("food-event-0001-of-0001.json") as file:
     food_event = json.load(file)
+
+# added mishaps and recalls
+added = open("added.txt", "w")
 
 # iterate through event file
 # check against recalls for similar product description to name_brand
@@ -29,18 +59,23 @@ with open("food-event-0001-of-0001.json") as file:
 name_brand_names = []
 i = 0
 for event in food_event["results"]:
+    print(event["date_started"])
+    name_brand_names.append(event["date_started"])
+    if not name_brand_names[0]:
+        print("true")
+    print(name_brand_names[0])
+    break
     for product in event["products"]:
         if product["name_brand"] not in name_brand_names:
             for recall in food_recall["results"]:
                 if compare_strings(product["name_brand"], recall["product_description"])[0] == 1 and \
                         compare_strings(product["name_brand"], recall["product_description"])[1] > 1:
-                            # add food event & recall to owl
+                            # add recall to owl
 
                             name_brand_names.append(product["name_brand"])
                             i += 1
-        # else:
-            # add food mishap
-
+    # add food mishap
+    # add_food_mishap(event, added)
 
 
 print(i)
